0d9b1f6eaadab908875bb1462f7b94e723b8eefd
Donovan Griffin <donovangriffin0214@gmail.com>
2024-07-09 09:44:55 -0400

    pwntools - updated solution

--- sol.py
+++ sol.py
@@ -1,8 +1,12 @@
 from pwn import *
 
 io = process('./time')
-io.sendline('echo Hello, world')
+#io.sendline('echo Hello, world')
 print(io.recvuntil("Enter your number: "))
 io.sendline(b'7')
 display = io.recvline()
 print(display)
+print(io.recvline())
+print(io.recvline())
+
+
